1bf0c058f7773622587dfbe2c8c51b8d8f39d92c
Sk26102001 <sushantkumar617@gmail.com>
2025-08-20 09:41:38 +0530

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,24 +1,13 @@
-// import React from "react";
-// import ReactDOM from "react-dom/client";
-// import { BrowserRouter as Router,Routes,Route } from "react-router-dom";
-// import App from "./App";
-// import "./index.css";
-
-// ReactDOM.createRoot(document.getElementById("root")).render(
-//   <BrowserRouter>
-//     <App />
-//   </BrowserRouter>
-// );
 
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>
+  </HashRouter>
 );
 
